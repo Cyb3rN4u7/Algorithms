710da9f727aec82c58fc0ffbdefc6115eaf5be38
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-09 22:51:32 +0200

    Implemented array based stack and tests

---
+++ src/Data_Structures/Stacks/StackArrayBased.ts
@@ -0,0 +1,36 @@
+
+class StackArrayBased{
+    items: any[];
+    constructor(){
+        this.items = [];
+    }
+
+    push(item:any){
+        this.items.push(item);
+    }
+
+    pop(){
+        return this.items.pop();
+    }
+
+    peek(){
+        return this.items[this.items.length -1];
+    }
+
+    isEmpty(){
+        return this.items.length === 0;
+    }
+
+    clear(){
+        this.items = [];
+    }
+
+    size(){
+        return this.items.length;
+    }
+
+
+
+}
+
+export default StackArrayBased;
